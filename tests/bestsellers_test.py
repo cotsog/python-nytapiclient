b7692f675a6c3c482bdcf550a73af8250fb78eab
Dominic Jodoin <cotsog@gmail.com>
2012-02-12 22:13:11 -0500

    - Added test and draft implementation for bestsellers' get_list API.

--- tests/bestsellers_test.py
+++ tests/bestsellers_test.py
@@ -4,9 +4,14 @@ from nytapiclient.bestsellers import BestSellers
 
 class BestSellersTest(unittest.TestCase):
 
+    def test_best_sellers_get_list(self):
+        b = BestSellers()
+        list = b.get_list('combined-print-and-e-book-fiction')
+        import pprint
+        pprint.pprint(list[0])
+        self.assertTrue(len(list) > 0)
+
     def test_best_sellers_get_list_names(self):
         b = BestSellers()
         list_names = b.get_list_names()
-        import pprint
-        pprint.pprint(list_names)
         self.assertTrue(len(list_names) > 0)
