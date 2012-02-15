3c9295c0882613b66a6a1667f082273e46dc7f42
Dominic Jodoin <cotsog@gmail.com>
2012-02-14 21:59:00 -0500

    API key is now passed to the constructor. Polished get_list().

--- nytapiclient/bestsellers.py
+++ nytapiclient/bestsellers.py
@@ -1,24 +1,27 @@
 import requests
 import simplejson
 
+
 class BestSellers:
 
-    __API_KEY = '9b10adbc72536a67930154a2bbcfedba:4:62210421'
     __API_DOMAIN = 'http://api.nytimes.com'
     __API_ENDPOINT_ROOT = '/svc/books'
     __API_VERSION = '/v2'
     __API_ENDPOINT_LIST = '/lists'
     __API_ENDPOINT_LIST_NAMES = '/lists/names'
-    
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
     def get_api_url(self, api_endpoint):
         return self.__API_DOMAIN            \
                 + self.__API_ENDPOINT_ROOT    \
                 + self.__API_VERSION        \
                 + api_endpoint
-    
+
     def append_api_key(self, url):
-        return url + '?api-key=' + self.__API_KEY
-    
+        return url + '?api-key=' + self.api_key
+
     def get_list_names(self):
         url = self.get_api_url(self.__API_ENDPOINT_LIST_NAMES)
         final_url = self.append_api_key(url)
@@ -26,21 +29,13 @@ class BestSellers:
         r = requests.get(final_url)
 
         j = simplejson.loads(r.content)
-        json_results = j[u'results']
+        return j[u'results']
 
-        results = []
-        for dict_item in json_results:
-            results.append(dict_item[u'list_name'])
-            
-        return results
-    
     def get_list(self, list_name):
         url = self.get_api_url(self.__API_ENDPOINT_LIST) + '/' + list_name
         final_url = self.append_api_key(url)
-        
+
         r = requests.get(final_url)
-        
+
         j = simplejson.loads(r.content)
-        
-        import pprint
-        pprint.pprint(j)
+        return j[u'results']
--- tests/bestsellers_test.py
+++ tests/bestsellers_test.py
@@ -4,14 +4,18 @@ from nytapiclient.bestsellers import BestSellers
 
 class BestSellersTest(unittest.TestCase):
 
-    def test_best_sellers_get_list(self):
-        b = BestSellers()
-        list = b.get_list('combined-print-and-e-book-fiction')
-        import pprint
-        pprint.pprint(list[0])
-        self.assertTrue(len(list) > 0)
+    __API_KEY = '9b10adbc72536a67930154a2bbcfedba:4:62210421'
 
-    def test_best_sellers_get_list_names(self):
-        b = BestSellers()
+    def test_best_sellers_get_book_list(self):
+        b = BestSellers(self.__API_KEY)
+        book_list = b.get_list('combined-print-and-e-book-fiction')
+        import pprint
+        pprint.pprint(book_list)
+        self.assertTrue(len(book_list) > 0)
+
+    def test_best_sellers_get_book_list_names(self):
+        b = BestSellers(self.__API_KEY)
         list_names = b.get_list_names()
+        import pprint
+        pprint.pprint(list_names)
         self.assertTrue(len(list_names) > 0)
